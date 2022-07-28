04efaac39d73ff512cd6394c51a252cbc2dc02f3
Maicon Jordan <maiconplus@gmail.com>
2022-07-28 20:37:55 -0300

    exercicio 1

---
+++ chute-um-numero.py
@@ -0,0 +1,16 @@
+#programa que iniciar um numero de 1 a 10 e o usuario deve acertar, o programa diz se ele chutou um numero a cima ou abaixo
+
+
+#numero = numero sorteado 1 a 10
+#acertou = false
+
+# enquanto aceritou = falso
+#chute = usuario digita um valor
+#if chute for maior numero imprime "chute maior que numero"
+#if  chute < numero
+#  "chute menor que numero"
+#if chute == numero
+#   print "voce acertou"
+#   acertou = true
+#else
+# voce digitou um numero invalido
