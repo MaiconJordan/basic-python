26e754f05a20c6a2650cb8453ce9d6a1dd82ad90
Maicon Jordan <maiconplus@gmail.com>
2022-07-28 21:05:21 -0300

    hota trabalhada

---
+++ valor-hora.py
@@ -0,0 +1,13 @@
+'''
+programa que retorna o valor da hora de trabalho de acordo com o salario e
+as horas de trabalho por mes, considerando os dias uteis =22
+'''
+
+
+salario = int(input("digite seu salario "))
+salario_trabalho = salario / 22
+hora = salario_trabalho / 8
+print(hora)
+
+
+
