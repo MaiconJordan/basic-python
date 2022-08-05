1024613cfe1589813df82609df8ad8c4a58ec1a5
Maicon Jordan <maiconplus@gmail.com>
2022-08-04 21:14:18 -0300

    melhorando o codigo

--- alura-py/forca.py
+++ alura-py/forca.py
@@ -1,26 +1,45 @@
+from re import A
+
+
 def jogar():
+    
     print("********************************")
     print("********** Jogo Forca **********")
     print("********************************")
     
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = "cabecuda".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
     
     enforcou = False
     acertou = False
+    erros = 0
+    
+    print(letras_acertadas)    
     
     while (not acertou and not enforcou):
         chute = input("Qual letra? ")
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
-    print("Jogando...")
+        
+        if (chute in palavra_secreta):        
+            index = 0
+            for letra in palavra_secreta:
+                if (chute.upper() == letra.upper()):
+                    letras_acertadas[index] = letra
+                index += 1
+            
+        else:
+            erros += 1 
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
         
         
-
+    if (acertou):
+        print("VOCÊ GANHOU ^_^ ")
+    else:
+        print("VOCÊ PERDEU")
     print("Fim do jogo")
 
 
