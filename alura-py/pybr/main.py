f212a6717e8b4f34000dd2a956c1cea6ab693286
Maicon Jordan <maiconplus@gmail.com>
2022-08-19 20:51:09 -0300

    melhorando metodos

--- alura-py/pybr/main.py
+++ alura-py/pybr/main.py
@@ -1,14 +1,4 @@
-from wsgiref import validate
 from Cpf import Cpf
-from validate_docbr import CPF
 
-cpf = CPF()
-
-print(cpf.validate("4456131651"))
-
-
-
-
-
-
-
+cpf_um = Cpf("15316264754")
+print(cpf_um)
