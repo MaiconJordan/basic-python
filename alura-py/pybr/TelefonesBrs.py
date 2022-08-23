27bb2d88708e106f0e5b3cfc05e03ab5d9c8b5de
Maicon Jordan <maiconplus@gmail.com>
2022-08-23 20:42:33 -0300

    formatando numero

--- alura-py/pybr/TelefonesBrs.py
+++ alura-py/pybr/TelefonesBrs.py
@@ -1,5 +1,6 @@
 import re
 
+
 class TelefonesBr:
     def __init__(self,telefone):
         if self.valida_telefone(telefone):
@@ -7,11 +8,21 @@ class TelefonesBr:
         else:
             raise ValueError("Numero incorreto !")
         
+    def __str__(self):
+        return self.numero_format()
+        
     def valida_telefone(self, telefone):
-        padrao = "[0-9]{2}[0-9]{2}[0-9]{4,5}[0-9]{4}"
+        padrao = "([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
         resposta = re.findall(padrao, telefone)
         if resposta:
             return True
         else:
             return False
-     
+        
+    def numero_format(self):
+        padrao = "([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})"
+        resposta = re.search(padrao, self.numero)
+        numero_formatado = "+{}({}){}-{}".format(
+            resposta.group(1), resposta.group(2), resposta.group(3), resposta.group(4)
+        )
+        return numero_formatado
--- alura-py/pybr/main.py
+++ alura-py/pybr/main.py
@@ -1,7 +1,8 @@
 from TelefonesBrs import TelefonesBr
 import re
 
-telefone = "552126481234"
+telefone = "55545999062759"
 
 telefone_objeto = TelefonesBr(telefone)
+
 print(telefone_objeto)
